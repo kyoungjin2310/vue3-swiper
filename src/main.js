d0d9ec91de87816785e3d34c77ecb7e2ad74140d
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-11-09 21:42:26 +0900

    feat:[다국어 적용] test

--- src/main.js
+++ src/main.js
@@ -6,10 +6,22 @@ import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import { createI18n } from 'vue-i18n';
 
+const i18n = createI18n({
+  locale: 'ko',
+  messages: {
+    en: {
+      signUp: 'Sign Up',
+    },
+    ko: {
+      signUp: '테스트 가입하기',
+    },
+  },
+});
 const vuetify = createVuetify({
   components,
   directives,
 });
 
-createApp(App).use(vuetify).use(VCalendar).use(router).mount('#app');
+createApp(App).use(vuetify).use(VCalendar).use(i18n).use(router).mount('#app');
