19b4c5e8e1531a69f6c8e4d26c113e90abe576ce
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-11-24 20:02:10 +0900

    feat: [textarea] textarea 입력값을 html로 변환

--- src/components/LangView.vue
+++ src/components/LangView.vue
@@ -7,6 +7,21 @@
     <div>
       {{ compare }}
     </div>
+    <div>
+      <textarea
+        cols="30"
+        rows="10"
+        ref="textboxObj"
+        @change="changeValue"
+        id="default"
+        :multiline="true"
+        :value="value"
+        floatLabelType="Auto"
+        placeholder="Enter your name"
+        class="sdsd"
+      ></textarea>
+    </div>
+    <div class="test" v-html="content"></div>
   </div>
 </template>
 
@@ -17,14 +32,32 @@ export default {
       startTime: '09:00',
       endTime: '10:00',
       date: new Date(),
+      value: 'Syncfusion',
     };
   },
   computed: {
     compare() {
       return this.startTime < this.endTime;
     },
+    content() {
+      return this.value.split('\n').join('<br>');
+    },
+  },
+  methods: {
+    changeValue: function () {
+      this.value = this.$refs.textboxObj.value;
+      console.log(this.$refs.textboxObj.value);
+    },
   },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.sdsd {
+  border: 1px solid #000;
+}
+.test {
+  width: 500px;
+  height: 200px;
+}
+</style>
