eae9944ac869bf1eb2bea19c1b0c3aa8d491b0d9
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-12-10 23:35:42 +0900

    loading test

--- src/mixin/mixin.js
+++ src/mixin/mixin.js
@@ -5,3 +5,18 @@ export const testCon = {
     },
   },
 };
+
+export const time = {
+  data() {
+    return {
+      loading: true,
+    };
+  },
+  methods: {
+    loadingHide(data) {
+      if (data) {
+        this.loading = false;
+      }
+    },
+  },
+};
--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,12 @@ export const store = createStore({
   state() {
     return {
       count: 1,
+      TimeDate: [
+        {
+          start: new Date.setHours(9, 30, 0),
+          end: new Date.setHours(23, 0, 0),
+        },
+      ],
     };
   },
 });
