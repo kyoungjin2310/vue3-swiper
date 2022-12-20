65223063c18659f06a4192025229de565dd85865
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-12-20 21:29:57 +0900

    [FormData] form data test

--- src/components/AddTag.vue
+++ src/components/AddTag.vue
@@ -25,18 +25,48 @@ export default {
       default: () => [],
     },
   },
-
-  watch: {
-    tags(n) {
-      this.$emit('input', n);
-    },
-  },
-
   data() {
     return {
       tag: '',
       tags: this.value || [],
+      params: new URLSearchParams({
+        arrayparams: ['val1', 'val2', 'val3'],
+      }),
+      otherParams: new URLSearchParams({}),
+      formData: new FormData(),
+      object: {},
     };
+  },
+  created() {
+    //otherParams
+    this.otherParams.append('arrayparams', 'val1');
+    this.otherParams.append('arrayparams', 'val2');
+    this.otherParams.append('arrayparams', 'val3');
+    console.log(
+      new URLSearchParams(this.otherParams).toString(),
+      'otherParams',
+    );
+
+    //배열 test - 안되는 것 같음
+    console.log(this.params.toString(), '배열 test');
+    const params = Object.fromEntries(this.params);
+    console.log(params, '배열 test');
+
+    //formData
+    this.formData.append('arrayparams', 'val1');
+    this.formData.append('arrayparams', 'val2');
+    this.formData.append('arrayparams', 'val3');
+    console.log(new URLSearchParams(this.formData).toString(), 'formData 1');
+    this.formData.forEach((value, key) => {
+      console.log(`key: ${key}    /    value: ${value},  formData`);
+      this.object = { ...this.object, [key]: `${value}` };
+    });
+    console.log(this.object, 'formData');
+  },
+  watch: {
+    tags(n) {
+      this.$emit('input', n);
+    },
   },
 
   methods: {
